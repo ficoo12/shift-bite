9f22a92fad563817875288b4b8004ed899fa1880
ficoo12 <101321862+ficoo12@users.noreply.github.com>
2025-11-19 13:16:49 +0100

    Update client.js

--- OwnerFrontend/src/api/client.js
+++ OwnerFrontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://shift-bite-backend.onrender.com",
   withCredentials: true,
 });
 
