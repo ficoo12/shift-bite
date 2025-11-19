9c5d121c75f970ab7bd25b531d2accc6c2d671e4
ficoo12 <101321862+ficoo12@users.noreply.github.com>
2025-11-19 13:56:58 +0100

    Update generateTokenAndSetCookie.js

--- backend/helperfunction/generateTokenAndSetCookie.js
+++ backend/helperfunction/generateTokenAndSetCookie.js
@@ -6,9 +6,9 @@ const generateTokenAndSetCookie = (res, userId) => {
   });
 
   res.cookie("token", token, {
-    httpOnly: true, //can not be accessed by JavaScrip because of httpOnly
-    secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
