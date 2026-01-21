edba577ab61657b8fe225e2fa71eb991e2763bb5
ficoo12 <101321862+ficoo12@users.noreply.github.com>
2026-01-21 19:31:37 +0100

    Update App.jsx

--- EmployeeApp/src/App.jsx
+++ EmployeeApp/src/App.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LogedIn from "./components/LogedIn";
 import { checkAuth } from "./features/auth/authSlice";
-import ErrorPage from "../../OwnerFrontend/src/components/ErrorPage";
+
 import Dashboard from "./components/Dashboard";
 import LogIn, { action as logInAction } from "./components/Login";
 import { action as logOutAction } from "./components/Logout";
@@ -12,14 +12,12 @@ function App() {
       path: "/",
       element: <LogedIn></LogedIn>,
       loader: checkAuth,
-      errorElement: <ErrorPage></ErrorPage>,
       children: [{ index: true, element: <Dashboard></Dashboard> }],
     },
     {
       path: "/login",
       element: <LogIn></LogIn>,
       action: logInAction,
-      errorElement: <ErrorPage></ErrorPage>,
     },
     {
       path: "/logout",
