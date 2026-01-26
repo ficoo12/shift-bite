9456ebc43dd8a5c01073bb52a38c06b9fe4c2c15
ficoo12 <101321862+ficoo12@users.noreply.github.com>
2026-01-26 19:22:42 +0100

    Update generateTokenAndSetCookie.js

--- backend/helperfunction/generateTokenAndSetCookie.js
+++ backend/helperfunction/generateTokenAndSetCookie.js
@@ -9,6 +9,7 @@ const generateTokenAndSetCookie = (res, userId) => {
     httpOnly: true,
     secure: true,
     sameSite: "none",
+    domain: ".onrender.com",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
